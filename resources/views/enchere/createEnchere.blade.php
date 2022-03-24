23f9ad81e8a4dda8482ac218f6efe63223b87253
Pandalefouque <97950670+Pandalefouque@users.noreply.github.com>
2022-03-24 16:03:46 +0100

    Update createEnchere.blade.php
    Mise en place de la vérification des dates

--- resources/views/enchere/createEnchere.blade.php
+++ resources/views/enchere/createEnchere.blade.php
@@ -8,12 +8,12 @@
         </header>
         <div class="card-content">
             <div class="content">
-                <form action="{{ route('enchere.store') }}" method="POST">
+                <form id="myForm" action="{{ route('enchere.store') }}" method="POST">
                     @csrf
                     <div class="field">
                         <label class="label">Nom de l'objet</label>
                         <div class="control">
-                            <input class="input" type="numeric" name="nom" value="{{ old('nom') }}">
+                            <input class="input" type="numeric" name="nom" required="required" value="{{ old('nom') }}">
                         </div>
                         @error('nom')
                         <p class="help is-danger">{{ $message }}</p>
@@ -36,7 +36,7 @@
                     <div class="field">
                         <label class="label">Prix de départ</label>
                         <div class="control">
-                            <input class="input" type="numeric" name="prix" value="{{ old('prix') }}">
+                            <input class="input" type="numeric" required="required" name="prix" value="{{ old('prix') }}">
                         </div>
                         @error('prix')
                         <p class="help is-danger">{{ $message }}</p>
@@ -46,7 +46,7 @@
                     <div class="field">
                         <label class="label">Date d'ouverture de l'enchère</label>
                         <div class="control">
-                            <input class="input" type="datetime-local" name="ouverture" value="{{ old('ouverture') }}">
+                            <input class="input" type="datetime-local" required="required" id="dateOuverture" name="ouverture" value="{{ old('ouverture') }}">
                         </div>
                         @error('ouverture')
                         <p class="help is-danger">{{ $message }}</p>
@@ -56,7 +56,7 @@
                     <div class="field">
                         <label class="label">Date de fermeture de l'enchère</label>
                         <div class="control">
-                            <input class="input" type="datetime-local" name="fermeture" value="{{ old('fermeture') }}">
+                            <input class="input" type="datetime-local" required="required" id="dateFermeture" name="fermeture" value="{{ old('fermeture') }}">
                         </div>
                         @error('fermeture')
                         <p class="help is-danger">{{ $message }}</p>
@@ -68,7 +68,7 @@
                     <input type="hidden" name="idProprietaire" value="{{Auth::user()->id}}"></input>
                     <div class="field">
                         <div class="control">
-                            <button class="button is-link">Envoyer</button>
+                            <button class="button is-link" type="button" onclick="ValidJS()">Envoyer</button>
 
                             <a class="button is-info" href="{{ route('objet.index') }}">Retour à la liste</a>
                         </div>
@@ -77,4 +77,39 @@
             </div>
         </div>
     </div>
+   <script type="text/javascript">
+
+
+       function verifDate(){
+           var valid = true;
+           var debut = document.getElementById("dateOuverture");
+           var fin = document.getElementById("dateFermeture");
+           var ddebut = debut.value.substr(0,10);
+           var Ddebut = debut.value.substr(11,5);
+           var dateDebut = new Date(ddebut+" "+Ddebut);
+           var dfin = fin.value.substr(0,10);
+           var Dfin = fin.value.substr(11,5);
+           var dateFin = new Date(dfin+" "+Dfin);
+           // alert(dateDebut.getTime() +" fin:"+ dateFin.getTime());
+           if(dateDebut.getTime() < dateFin.getTime())
+           {
+               // window.alert("c'est good");
+           }
+           else{
+               window.alert("les dates ne sont pas ordonnées");
+               valid = false;
+           }
+           return valid;
+       }
+
+       function ValidJS(){
+           var form = document.forms["myForm"];
+           var valid = verifDate();
+           if(valid){
+               form.submit();
+           }
+
+       }
+
+   </script>
 @endsection
